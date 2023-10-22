96985f35b0ded1249099248224ab4bdb1a417d74
Mariana Capuñay <mariana.capunay@utec.edu.pe>
2023-10-22 00:09:36 -0500

    update readIndex in GlobalIndex

--- InvertedIndex/Global_Index/read_index.py
+++ InvertedIndex/Global_Index/read_index.py
@@ -1,7 +1,6 @@
 import json
-import sys 
-sys.path.append('../')
-from rutas import ruta_indices
+
+ruta_indices = r"C:\Users\ASUS\OneDrive - UNIVERSIDAD DE INGENIERIA Y TECNOLOGIA\Escritorio\bd2_proyecto_2023.2\proyecto_2\Project2_db2\InvertedIndex\Local_Index"
 
 
 def read_json(nombre_archivo:str)->dict:
@@ -9,4 +8,12 @@ def read_json(nombre_archivo:str)->dict:
         result = json.load(archivo)
     return result
 
-print(read_json("index01.json"))
+def read_index(nro_index:int)->dict:
+    nro_index_str:str = str(nro_index) #convierte el nro de index a string
+
+    if nro_index<10: #caso en el que se debe añadir un cero al inicio
+        nro_index_str = "0"+nro_index_str
+    return read_json("index"+nro_index_str+".json")
+
+print("Indice 1:", read_index(1))
+print("Indice 2:", read_index(2))
--- InvertedIndex/main.py
+++ InvertedIndex/main.py
@@ -1,12 +1,5 @@
 from InvertedIndex import InvertedIndex
 
-"""
-dicc = {}
-dicc["termino1"] = "hola"
-dicc["termino2"] = {2:3} 
-dicc["termino2"][2] +=5
-print(dicc)
-"""
 a = InvertedIndex()
 
 #a.preProcessandIndex("el perro corriendo. Gato corre")
