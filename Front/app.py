30a5f0e473313e43617982f8d33fc988960c9ed6
manyory <manyory.cueva@utec.edu.pe>
2023-10-29 11:45:54 -0500

    app_update

--- Front/app.py
+++ Front/app.py
@@ -1,4 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session, jsonify
+import time
+import psycopg2
+import psql_connection as psql
+
 
 app = Flask(__name__)
 
@@ -20,13 +24,38 @@ def home():
 def about():
     return render_template("about.html")
 
-@app.route('/indice-invertido')
-def todas_ropas():
-    return render_template("indice-invertido.html")
+@app.route('/consulta', methods=['POST'])
+def resultados():
+    query = request.form.get('query')
+    topk = request.form.get('topk')
+    action = request.form.get('action')
+
+
+
+    #return render_template("resultados.html", resultados=resultados, tiempo_ejecucion=tiempo_ejecucion)
+
+
+    if action == 'Indice':
+        return jsonify({"message": "Se ha creado el índice exitosamente"})
+
+    elif action == 'PgAdmin':
+        #llamar a la funcion de consulta
+        start_time = time.time()
+        psql.consulta()
+        end_time = time.time()
+        tiempo_ejecucion = end_time - start_time
+        #mostrar los resultados
+        #return render_template("resultados.html", resultados=resultados, tiempo_ejecucion=tiempo_ejecucion)
+        return jsonify({"message": "Se ha creado el índice exitosamente", "tiempo_ejecucion": tiempo_ejecucion})
 
 @app.route('/PgAdmin')
 def pgAdmin():
     return render_template("pgAdmin.html")
+
+@app.errorhandler(404)
+def page_not_found():
+    return jsonify({"message": "Página no encontrada, :( )"}), 404
+
 # Make sure this we are executing this file
 if __name__ == '__main__':
     app.run(debug=True)
